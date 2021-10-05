4a53b337d1a4636b56d93926af0e08015d6e8613
Savov,Kristian <k.savov@student.fontys.nl>
2021-10-05 11:39:27 +0200

    project init

---
+++ backend/src/main/java/fontys/crossyn/CrossynApplication.java
@@ -0,0 +1,13 @@
+package fontys.crossyn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrossynApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrossynApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/fontys/crossyn/CrossynApplicationTests.java
@@ -0,0 +1,13 @@
+package fontys.crossyn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrossynApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
