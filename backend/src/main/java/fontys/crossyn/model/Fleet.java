cc046dd5570e93f1eef6ee2cc8ddd85759506c31
Preslavski,Kaloyan K.S <k.preslavski@student.fontys.nl>
2021-10-13 12:21:44 +0200

    fleet, vehicle, user classes created

---
+++ backend/src/main/java/fontys/crossyn/model/Fleet.java
@@ -0,0 +1,23 @@
+package fontys.crossyn.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Fleet {
+
+    @Id @GeneratedValue private long id;
+    private List<Vehicle> vehicles = new ArrayList<Vehicle>();
+    private User user;
+    private String name;
+    private long userId = user.getId();
+
+}
