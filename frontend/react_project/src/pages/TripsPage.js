f5dacc5bf3018a90a19df7b9798912d699ffb90e
Preslavski,Kaloyan K.S <k.preslavski@student.fontys.nl>
2021-12-13 20:11:12 +0100

    columns

--- frontend/react_project/src/pages/TripsPage.js
+++ frontend/react_project/src/pages/TripsPage.js
@@ -1,6 +1,70 @@
 import React from 'react';
 import { DataGrid } from '@mui/x-data-grid';
 
+const columns = [
+  {
+    field: 'id',
+    headerName: 'ID',
+    width: 70,
+  },
+  {
+    field: 'vehicleId',
+    headerName: 'Vehicle ID',
+    width: 70,
+  },
+  {
+    field: 'startTime',
+    headerName: 'Start time',
+    width: 70,
+  },
+  {
+    field: 'endTime',
+    headerName: 'End time',
+    width: 70,
+  },
+  {
+    field: 'startLoc',
+    headerName: 'Start location',
+    width: 70,
+  },
+  {
+    field: 'endLoc',
+    headerName: 'End location',
+    width: 70,
+  },
+];const columns = [
+  {
+    field: 'id',
+    headerName: 'ID',
+    width: 70,
+  },
+  {
+    field: 'vehicleId',
+    headerName: 'Vehicle ID',
+    width: 70,
+  },
+  {
+    field: 'startTime',
+    headerName: 'Start time',
+    width: 70,
+  },
+  {
+    field: 'endTime',
+    headerName: 'End time',
+    width: 70,
+  },
+  {
+    field: 'startLoc',
+    headerName: 'Start location',
+    width: 70,
+  },
+  {
+    field: 'endLoc',
+    headerName: 'End location',
+    width: 70,
+  },
+];
+
 const TripsPage = () => {
   return (
     <div>
