054cef526d8c3334c89b0cebae1077a51b1555da
Preslavski,Kaloyan K.S <k.preslavski@student.fontys.nl>
2021-12-13 20:12:35 +0100

    columns

--- frontend/react_project/src/pages/TripsPage.js
+++ frontend/react_project/src/pages/TripsPage.js
@@ -32,37 +32,6 @@ const columns = [
     headerName: 'End location',
     width: 70,
   },
-];const columns = [
-  {
-    field: 'id',
-    headerName: 'ID',
-    width: 70,
-  },
-  {
-    field: 'vehicleId',
-    headerName: 'Vehicle ID',
-    width: 70,
-  },
-  {
-    field: 'startTime',
-    headerName: 'Start time',
-    width: 70,
-  },
-  {
-    field: 'endTime',
-    headerName: 'End time',
-    width: 70,
-  },
-  {
-    field: 'startLoc',
-    headerName: 'Start location',
-    width: 70,
-  },
-  {
-    field: 'endLoc',
-    headerName: 'End location',
-    width: 70,
-  },
 ];
 
 const TripsPage = () => {
