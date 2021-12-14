8fb7b3b027965d8f62c1a0d2b223b4d9a7cf8a00
TretiqHiks <tiggerforrce53@gmail.com>
2021-12-14 22:12:10 +0100

    url's allowed

--- backend/src/main/java/fontys/crossyn/config/AuthenticationConfiguration.java
+++ backend/src/main/java/fontys/crossyn/config/AuthenticationConfiguration.java
@@ -1,5 +1,7 @@
 package fontys.crossyn.config;
 
+import javax.print.DocFlavor;
+
 public class AuthenticationConfiguration {
     public static final String SECRET = "BigSecret";
     public static final long EXPIRATION_TIME = 2 * 60 * 1000; // 1 minute
@@ -11,6 +13,9 @@ public class AuthenticationConfiguration {
 
 
     //URLs
+    public static final String TRIPS_URL = "api/trips";
+    public static final String GET_TRIPS_URL = TRIPS_URL + "/GetAll";
+    public static final String CREATE_TRIPS_URL = TRIPS_URL + "/CreateTrips";
     public static final String SIGN_UP_URL = "/account";
     public static final String REFRESH_URL = SIGN_UP_URL + "/token/refresh";
     public static final String LOGIN_URL = "/login";
